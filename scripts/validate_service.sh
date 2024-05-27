b907d560c87f0ffb7ebc3ed84e789ad1e1f649ab
Lokesh350 <lokeshrajagopal2@gmail.com>
2024-05-27 14:18:48 +0530

    commit

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-set -e
-
-# Service name
-SERVICE_NAME="my-service"
-
-# Check if service exists
-if sudo systemctl list-unit-files | grep -q "$SERVICE_NAME.service"; then
-    # Check if service is active
-    if sudo systemctl is-active --quiet $SERVICE_NAME; then
-        echo "Service $SERVICE_NAME is active."
-        exit 0
-    else
-        echo "ERROR: Service $SERVICE_NAME is not active."
-        exit 1
-    fi
-else
-    echo "Service $SERVICE_NAME does not exist."
-    exit 0
-fi
+cd /var/www/html
+npm start
